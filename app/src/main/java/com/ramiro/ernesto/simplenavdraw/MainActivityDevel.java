388ebe7b5c7d36496f08779cd5029b8396e6848d
Ernesto Ramiro <atomms@gmail.com>
2016-12-21 20:01:29 +0100

    new MainActivityDevel

--- app/src/main/java/com/ramiro/ernesto/simplenavdraw/MainActivityDevel.java
+++ app/src/main/java/com/ramiro/ernesto/simplenavdraw/MainActivityDevel.java
@@ -1,34 +1,41 @@
 package com.ramiro.ernesto.simplenavdraw;
 
-import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
 /**
- * @version 0.0.1
+ * Created by ernesto on 21/12/16.
  */
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+import android.support.v7.app.AppCompatActivity;
+
+
+        import android.os.Bundle;
+        import android.support.design.widget.NavigationView;
+        import android.support.design.widget.TabLayout;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.design.widget.Snackbar;
+        import android.support.v4.app.FragmentTransaction;
+        import android.view.View;
+        import android.support.design.widget.NavigationView;
+        import android.support.v4.view.GravityCompat;
+        import android.support.v4.widget.DrawerLayout;
+        import android.support.v7.app.ActionBarDrawerToggle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentManager;
+        import android.support.v4.app.FragmentPagerAdapter;
+        import android.support.v4.view.ViewPager;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+
+        import static android.R.attr.fragment;
+
+public class MainActivityDevel extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -62,6 +69,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_menu_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_menu_gallery);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_menu_slideshow);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_menu_share);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -120,15 +131,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment;
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            mViewPager.setCurrentItem(1);
         } else if (id == R.id.nav_slideshow) {
-
+            mViewPager.setCurrentItem(2);
         } else if (id == R.id.nav_manage) {
-
+            mViewPager.setCurrentItem(3);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
